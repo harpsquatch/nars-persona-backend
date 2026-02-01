4d2364fcd6641fd2f13fff4db1d4690971e39c92
harpsquatch <harpsquatch@users.noreply.github.com>
2026-02-01 16:33:43 +0530

    Change backend port from 8000 to 8080

--- app.py
+++ app.py
@@ -1437,4 +1437,4 @@ def create_app(config_name='development'):
 app = create_app('production')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8080)
